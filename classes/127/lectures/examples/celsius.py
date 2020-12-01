7a8cc45bc5fd1a5682eac3b34f0df602758d1062
reesep <reesepearsallcs@gmail.com>
2020-12-01 05:43:58 -0700

    add more day 2 stuff

---
+++ classes/127/lectures/examples/celsius.py
@@ -0,0 +1,13 @@
+
+#Write a program that will convert farenheit to
+# celsius
+
+# 1. get input from user
+# 2. calculate using the formula
+# 3. print out answer
+
+farenheit = int(input("Enter a temp in F: "))
+
+celsius =  (farenheit - 32) * (5/9)
+
+print(celsius)
