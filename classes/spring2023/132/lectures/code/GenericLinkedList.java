6fc8fe6da2e9d7162a1c79ea868720d4ae695051
Reese Pearsall <reesepearsallcs@gmail.com>
2023-05-05 17:11:24 -0600

    push for friday 5/5

---
+++ classes/spring2023/132/lectures/code/GenericLinkedList.java
@@ -0,0 +1,54 @@
+
+public class GenericLinkedList {
+
+	public class Node<E>{
+		
+		E data;
+		Node<E> next;
+		
+		public Node(E data) {
+			this.data = data;
+			this.next = null;
+		}
+		
+		public E getData() {
+			return this.data;
+		}
+		
+		public Node getNext() {
+			return this.next;
+		}
+		
+	}
+	
+	//start of linked list class
+	
+	private Node head;
+	private int size;
+	
+	public GenericLinkedList() {
+		this.head = null;
+		this.size = 0;
+	}
+	
+	public <E> void add(E newData) {
+		
+		Node<E> newNode = new Node<E>(newData);
+		newNode.next = this.head;
+		this.head = newNode;
+		
+	}
+	
+	public void print() {
+		Node current = this.head;
+		while(current != null) {
+			System.out.println(current.getData());
+			current = current.getNext();
+		}
+		
+		
+	}
+	
+	
+	
+}
---
+++ classes/spring2023/132/lectures/code/GenericLinkedListDemo.java
@@ -0,0 +1,16 @@
+
+public class GenericLinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		GenericLinkedList list1 = new GenericLinkedList();
+		
+		list1.add(111);
+		list1.add("reese");
+		list1.add(2.0);
+		
+		list1.print();
+
+	}
+
+}
